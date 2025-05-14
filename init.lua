52e574bfeb1ac137638ecb703d12230141dd3ee7
alu-cg <codeerror0x01@gmail.com>
2025-05-14 19:49:59 +0800

    enable python lsp

--- init.lua
+++ init.lua
@@ -647,7 +647,7 @@ require('lazy').setup({
           },
         },
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
